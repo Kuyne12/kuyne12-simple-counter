3e75bf23aaaf506d9dc2e749b786a7ccba7e80d9
Kuyne12 <124099764+Kuyne12@users.noreply.github.com>
2023-04-22 20:33:31 +0000

    updated prject

--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,48 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+//include bootstrap npm library into the bundle
+import 'bootstrap';
+
+import 'icons.js';
+
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
+
+function SimpleCounter(props){
+    return (<div> className="bigCounter">
+        <div className="calender">
+            <i class="fa-regular fa-clock"></i>
+        </div>
+        <div className="four">0</div>
+        <div className="three">0</div>
+        <div className="two">0</div>
+        <div className="one">0</div>
+   <div>);
+}
+SimpleCounter.propTypes = {
+    digitFour: propTypes.number,
+    digitThree: propTypes.number,
+    digitTwo: propTypes.number,
+    digitOne: propTypes.number,
+};
+
+let counter = 0;
+setInterval(function)(){
+    const four = Math.floor(counter/10000);
+    const three = Math.floor(counter/1000);
+    const two = Math.floor(counter/100);
+    const one = Math.floor(counter/10);
+    console.log(four, three, two, one);
+    //render your react application
+    counter++;
+},1000);
+
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(
+   <SimpleCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} />,
+   document.querySelector("#app")
+  );
+},
